f53ae4df73854fae7bf8ad02ec4c98eb31164dee
sambeck87 <sandro_mipb@hotmail.com>
2023-01-23 15:07:39 -0600

    Setup books.js

---
+++ src/redux/books/books.js
@@ -0,0 +1,28 @@
+const ADD_BOOK = 'BookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'BookStore/books/REMOVE_BOOK';
+
+const initalState = [];
+
+export const add = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
+
+export const remove = (book) => ({
+  type: REMOVE_BOOK,
+  id: book.id,
+});
+
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    case ADD_BOOK: return [
+      ...state,
+      action.book
+    ];
+    case REMOVE_BOOK: return state.filter((item) => item.id !== action.id);
+
+    default: return state;
+  }
+};
+
+export default reducer;
